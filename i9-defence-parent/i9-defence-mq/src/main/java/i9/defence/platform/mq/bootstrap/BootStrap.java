4dce690a80db5558ae52e775feef4c2c91ce1134
Administrator <Administrator@2013-20150930QB>
2018-01-17 13:21:23 +0800

    添加线程管理

--- i9-defence-parent/i9-defence-mq/src/main/java/i9/defence/platform/mq/bootstrap/BootStrap.java
+++ i9-defence-parent/i9-defence-mq/src/main/java/i9/defence/platform/mq/bootstrap/BootStrap.java
@@ -1,13 +1,12 @@
 package i9.defence.platform.mq.bootstrap;
 
-import javax.jms.TextMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import i9.defence.platform.mq.libraries.ConsumerService;
+import i9.defence.platform.mq.pool.BusinessPool;
 import i9.defence.platform.mq.utils.SpringBeanService;
 
 @SuppressWarnings("serial")
@@ -19,21 +18,7 @@ public class BootStrap extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         logger.info("消息队列服务器已启动");
-        new Thread(new Runnable() {
-            
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        ConsumerService consumerService = SpringBeanService.getBean(ConsumerService.class);
-                        TextMessage textMessage = consumerService.receive();
-                        if (textMessage != null) {
-                        }
-                    }
-                    catch (Exception e) {
-                    }
-                }
-            }
-        }).start();
+        BusinessPool businessPool = SpringBeanService.getBean(BusinessPool.class);
+        businessPool.startTask();
     }
 }
