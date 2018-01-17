72ff929f21366291332e5286e5ec73337ed6d6fa
姜涛\Administrator <jiangtao19890902@sina.com>
2018-01-17 16:25:47 +0800

    增加MQ消息队列实现

---
+++ i9-defence-parent/i9-defence-mq/src/main/java/i9/defence/platform/mq/service/ConsumerService.java
@@ -0,0 +1,28 @@
+package i9.defence.platform.mq.service;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    @Resource(name = "jmsTemplate")
+    private JmsTemplate jmsTemplate;
+
+    public TextMessage receive(Destination destination) {
+        TextMessage tm = (TextMessage) jmsTemplate.receive(destination);
+        if (tm != null) {
+            try {
+                System.out.println("Get Message: " + tm.getText() + " from Destination " + destination.toString());
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+        return tm;
+    }
+}
---
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/ProducerService.java
@@ -0,0 +1,39 @@
+package i9.defence.platform.socket.service;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+    @Resource(name = "jmsTemplate")
+    private JmsTemplate jmsTemplate;
+
+    public void sendMessage(Destination destination, final String msg) {
+        System.out.println("Send " + msg + " to Destination " + destination.toString());
+        MessageCreator messageCreator = new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage(msg);
+            }
+        };
+        jmsTemplate.send(destination, messageCreator);
+    }
+
+    public void sendMessage(final String msg) {
+        String destination = jmsTemplate.getDefaultDestinationName().toString();
+        System.out.println("Send " + msg + " to Destination " + destination);
+        MessageCreator messageCreator = new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage(msg);
+            }
+        };
+        jmsTemplate.send(messageCreator);
+    }
+}
