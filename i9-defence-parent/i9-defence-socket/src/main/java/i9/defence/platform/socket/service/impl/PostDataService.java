33754c0bd4402c5946ccad462203e1f2147dc59d
姜涛\Administrator <jiangtao19890902@sina.com>
2018-01-30 13:14:18 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/PostDataService.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/PostDataService.java
@@ -3,6 +3,7 @@ package i9.defence.platform.socket.service.impl;
 import org.springframework.stereotype.Service;
 
 import i9.defence.platform.socket.context.ChannelPacker;
+import i9.defence.platform.socket.message.req.UplinkReqMessage;
 import i9.defence.platform.socket.netty.Message;
 import i9.defence.platform.socket.service.ICoreService;
 
@@ -11,5 +12,6 @@ public class PostDataService implements ICoreService {
 
     @Override
     public void doPost(Message message, ChannelPacker channelPacker) {
+        UplinkReqMessage uplinkReqMessage = (UplinkReqMessage) message.getMessageDecodeConvert();
     }
 }
