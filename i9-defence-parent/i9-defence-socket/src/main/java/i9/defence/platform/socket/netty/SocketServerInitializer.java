6400019f045e81ba11121270b70814e704b2dc73
姜涛\Administrator <jiangtao19890902@sina.com>
2018-02-03 16:02:45 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/SocketServerInitializer.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/SocketServerInitializer.java
@@ -3,9 +3,12 @@ package i9.defence.platform.socket.netty;
 import i9.defence.platform.socket.netty.codec.MessageDecoder;
 import i9.defence.platform.socket.netty.codec.MessageEncoder;
 import i9.defence.platform.socket.netty.handler.ServiceHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.timeout.IdleStateHandler;
 
 public class SocketServerInitializer extends ChannelInitializer<SocketChannel> {
@@ -13,9 +16,13 @@ public class SocketServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast("idleStateHandler", new IdleStateHandler(3600, 3600, 3600));
-        pipeline.addLast("crcDecoder", new MessageDecoder());
-        pipeline.addLast("transferServerHandler", new ServiceHandler());
-        pipeline.addLast("crcEncoder", new MessageEncoder());
+        pipeline.addLast(new IdleStateHandler(3600, 3600, 3600));
+        ByteBuf delimiter = Unpooled.copiedBuffer("#".getBytes());
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(2048,delimiter));    
+        // 字符串解码 和 编码
+        pipeline.addLast("decoder", new MessageDecoder());
+        pipeline.addLast("encoder", new MessageEncoder());
+        // 自己的逻辑Handler
+        pipeline.addLast("handler", new ServiceHandler());
     }
 }
--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/codec/MessageDecoder.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/codec/MessageDecoder.java
@@ -5,55 +5,58 @@ import i9.defence.platform.socket.message.req.HeartbeatReqMessage;
 import i9.defence.platform.socket.message.req.LoginReqMessage;
 import i9.defence.platform.socket.message.req.UplinkReqMessage;
 import i9.defence.platform.socket.netty.Message;
+import i9.defence.platform.socket.util.EncryptUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageDecoder;
 
 import java.util.List;
 
-public class MessageDecoder extends ByteToMessageDecoder {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageDecoder extends MessageToMessageDecoder<ByteBuf> {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(MessageDecoder.class);
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf buf, List<Object> list) throws Exception {
-        if (buf.readableBytes() < 1 + 1 + 1 + 4 + 1 + 1) {
-            return;
-        }
-        buf.markReaderIndex();
-        buf.skipBytes(2);
+        byte start = buf.readByte();
+        byte version = buf.readByte();
 
         byte type = buf.readByte();
-        buf.readInt();
-        
+        int index = buf.readInt();
+
         int len = buf.readableBytes();
-        
-        if (buf.readableBytes() <= (1 + 1)) { //读到的消息体长度如果小于我们传送过来的消息长度，则resetReaderIndex. 这个配合markReaderIndex使用的。把readIndex重置到mark的地方
-            buf.resetReaderIndex();
-            return;
-        }
-        
-        byte[] dst = new byte[len - 1 - 1];
+
+        len -= 2;
+        byte[] dst = new byte[len];
         buf.readBytes(dst);
-        
+
+        byte sumcheck = buf.readByte();
+        byte end = buf.readByte();
+
+        logger.info(
+                "解码, 起始符 : {}, 协议版本号 : {}, 消息类型 : {}, 消息索引 : {}, data : {}, 校验和 : {}, 结束符 : {}",
+                String.format("%02X", start), String.format("%02X", version),
+                String.format("%02X", type), index,
+                EncryptUtils.bytesToHexString(dst),
+                String.format("", sumcheck), String.format("%02X", end));
+
         MessageDecodeConvert messageDecodeConvert = null;
         if (type == 0x00) {
             messageDecodeConvert = new LoginReqMessage();
-        }
-        else if (type == 0xFF) {
+        } else if (type == 0xFF) {
             messageDecodeConvert = new HeartbeatReqMessage();
-        }
-        else {
+        } else {
             messageDecodeConvert = new UplinkReqMessage();
         }
-        
-        buf.readByte();
-        buf.readByte();
-        
         ByteBuf buf0 = Unpooled.buffer(len);
         buf0.writeBytes(dst);
-        
         messageDecodeConvert.decode(buf0);
-        
+
         Message message = new Message();
         message.setType(type);
         message.setMessageDecodeConvert(messageDecodeConvert);
