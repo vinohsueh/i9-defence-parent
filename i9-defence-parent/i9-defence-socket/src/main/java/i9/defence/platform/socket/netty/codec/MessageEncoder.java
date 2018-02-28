c35a57c928eaac2a76471fbc8f44de6f67b7ff1e
Administrator <Administrator@2013-20150930QB>
2018-02-28 08:47:15 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/codec/MessageEncoder.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/netty/codec/MessageEncoder.java
@@ -1,23 +1,21 @@
 package i9.defence.platform.socket.netty.codec;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import i9.defence.platform.socket.util.EncryptUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.MessageToByteEncoder;
 
-public class MessageEncoder extends MessageToMessageEncoder<ByteBuf> {
+public class MessageEncoder extends MessageToByteEncoder<ByteBuf> {
     
     @Override
-    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
-        byte[] dst = new byte[msg.readableBytes()];
-        msg.readBytes(dst);
-        out.add(dst);
-        logger.info("发送数据, data : {}", EncryptUtils.bytesToHexString(dst));
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf buf) throws Exception {
+        // 处理通信编码
+        byte[] data = msg.array();
+        buf.writeBytes(data);
+        logger.info("发送数据, data : {}", EncryptUtils.bytesToHexString(data));
     }
     
     private static final Logger logger = LoggerFactory.getLogger(MessageEncoder.class);
