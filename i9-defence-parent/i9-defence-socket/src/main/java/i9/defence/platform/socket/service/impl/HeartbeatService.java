cc7bcf4736a5220ca40aa1715229a0476ec43635
Administrator <Administrator@2013-20150930QB>
2018-03-05 09:03:52 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/HeartbeatService.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/HeartbeatService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import i9.defence.platform.socket.context.ChannelPacker;
+import i9.defence.platform.socket.message.req.HeartbeatReqMessage;
+import i9.defence.platform.socket.message.resp.HeartbeatRespMessage;
 import i9.defence.platform.socket.netty.Message;
 import i9.defence.platform.socket.service.ICoreService;
 
@@ -13,7 +15,11 @@ public class HeartbeatService implements ICoreService {
 
     @Override
     public void doPost(Message message, ChannelPacker channelPacker) {
-        logger.info("heartbeat channelId : " + channelPacker.getChannelId());
+        HeartbeatReqMessage heartbeatReqMessage = (HeartbeatReqMessage) message.getMessageDecodeConvert();
+        logger.info("heartbeat channelId : " + channelPacker.getChannelId() + ", timestamp : " + heartbeatReqMessage.timestamp);
+        int timestamp = (int) (System.currentTimeMillis() / 1000);
+        HeartbeatRespMessage heartbeatRespMessage = new HeartbeatRespMessage(timestamp);
+        message.setMessageEncodeConvert(heartbeatRespMessage);
     }
     
     private static final Logger logger = LoggerFactory.getLogger(HeartbeatService.class);
