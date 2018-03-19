47a031af0be88c4f6a9c8d043866dc7d647ee504
28937 <28937@DESKTOP-4NLT654.lan>
2018-03-19 17:58:12 +0800

    1

---
+++ i9-defence-parent/i9-defence-service/src/main/java/i9/defence/platform/service/StreamOriginService.java
@@ -0,0 +1,22 @@
+package i9.defence.platform.service;
+
+import i9.defence.platform.dao.vo.StreamOriginSearchDto;
+import i9.defence.platform.model.StreamOrigin;
+import i9.defence.platform.utils.BusinessException;
+import i9.defence.platform.utils.PageBounds;
+
+/**
+ * Service
+ * @author gbq
+ * @create 2018年3月19日
+ */
+public interface StreamOriginService {
+	  /**
+     * 分页查询项目
+     * @param streamOriginSearchDto
+     * @param currectPage
+     * @param pageSize
+     * @return
+     */
+    PageBounds<StreamOrigin> selectByLimitPage(StreamOriginSearchDto streamOriginSearchDto) throws BusinessException;
+}
---
+++ i9-defence-parent/i9-defence-service/src/main/java/i9/defence/platform/service/impl/StreamOriginServiceImpl.java
@@ -0,0 +1,30 @@
+package i9.defence.platform.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import i9.defence.platform.dao.StreamOriginDao;
+import i9.defence.platform.dao.vo.StreamOriginSearchDto;
+import i9.defence.platform.model.StreamOrigin;
+import i9.defence.platform.service.StreamOriginService;
+import i9.defence.platform.utils.BusinessException;
+import i9.defence.platform.utils.PageBounds;
+@Service
+@Transactional
+public class StreamOriginServiceImpl implements StreamOriginService {
+
+	@Autowired
+	private StreamOriginDao streamOriginDao;
+	
+	@Override
+	public PageBounds<StreamOrigin> selectByLimitPage(StreamOriginSearchDto streamOriginSearchDto)
+			throws BusinessException {
+		try {
+			return streamOriginDao.selectByLimitPage(streamOriginSearchDto, streamOriginSearchDto.getCurrentPage(), streamOriginSearchDto.getPageSize());
+		} catch (Exception e) {
+			throw new BusinessException("分页项目类别类别查询失败",e.getMessage());
+		}
+	}
+
+}
