f8a57a6ee11682394b14e21a91d166feb8b83e7b
姜涛 <jiangtao19890902@sina.com>
2018-03-24 13:44:28 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/UpStreamService.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/UpStreamService.java
@@ -1,7 +1,5 @@
 package i9.defence.platform.socket.service.impl;
 
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
-import i9.defence.platform.model.StreamOrigin;
 import i9.defence.platform.mq.libraries.business.BusinessProducerService;
 import i9.defence.platform.netty.libraries.DataParseUtil;
 import i9.defence.platform.netty.libraries.EncryptUtils;
 import i9.defence.platform.netty.libraries.req.DataMessage;
 import i9.defence.platform.netty.libraries.req.UpStreamReqMessage;
-import i9.defence.platform.service.UpStreamOriginService;
 import i9.defence.platform.socket.context.ChannelPacker;
 import i9.defence.platform.socket.netty.Message;
 import i9.defence.platform.socket.service.ICoreService;
 
 @Service
 public class UpStreamService implements ICoreService {
-	
-	@Autowired
-	private UpStreamOriginService streamOriginService;
-	
+
     @Override
     public void doPost(Message message, ChannelPacker channelPacker) {
         UpStreamReqMessage upStreamReqMessage = (UpStreamReqMessage) message.getMessageDecodeConvert();
@@ -37,16 +30,7 @@ public class UpStreamService implements ICoreService {
                     + ", 值 : " + DataParseUtil.parseDataValue(dataMessage.type, dataMessage.data));
         }
         JSONObject jsonObject = upStreamReqMessage.toJSONObject();
-        
-        // TODO 保存json数据 service -> dao - db
-        // id jsonstr submitDate
-        // t_up_stream_origin
-        // t_up_stream_decode
         String jsonStr = jsonObject.toJSONString();
-        StreamOrigin streamOrigin = new StreamOrigin();
-        streamOrigin.setJsonstr(jsonStr);
-        streamOrigin.setSubmitDate(new Date());
-        streamOriginService.addStreamOrigin(streamOrigin);
         try {
             producerService.sendMessage(jsonStr);
             logger.info("发送到MQ服务器消息成功, jsonStr : " + jsonStr);
