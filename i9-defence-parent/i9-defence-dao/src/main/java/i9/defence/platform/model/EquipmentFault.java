8e139e3d59b1df486e12d5e948743658afd102bf
TheDazzlings <1019075338@qq.com>
2018-03-29 11:41:35 +0800

    2018年3月29日 11:41:23

---
+++ i9-defence-parent/i9-defence-dao/src/main/java/i9/defence/platform/model/EquipmentFault.java
@@ -0,0 +1,64 @@
+package i9.defence.platform.model;
+
+public class EquipmentFault {
+    /**
+     * id
+     */
+    private Integer id;
+    /**
+     * 故障名称
+     */
+    private String name;
+    /**
+     * 故障代码
+     */
+    private String code;
+    /**
+     * 设备id
+     */
+    private Integer equipmentId;
+    /**
+     * 设备名称
+     */
+    private String equipmentName;
+    
+    public String getEquipmentName() {
+		return equipmentName;
+	}
+
+	public void setEquipmentName(String equipmentName) {
+		this.equipmentName = equipmentName;
+	}
+
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public Integer getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(Integer equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+}
