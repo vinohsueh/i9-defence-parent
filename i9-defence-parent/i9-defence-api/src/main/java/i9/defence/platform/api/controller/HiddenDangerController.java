e63e966889e3dfd22e737891bf9d48950e8d454e
vinohsueh <413311010@qq.com>
2018-04-10 11:49:09 +0800

    1

--- i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/controller/HiddenDangerController.java
+++ i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/controller/HiddenDangerController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import i9.defence.platform.dao.vo.PageListDto;
