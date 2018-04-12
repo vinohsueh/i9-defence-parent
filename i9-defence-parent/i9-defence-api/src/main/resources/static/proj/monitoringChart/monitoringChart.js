40f7a6d9595f5593412c48ec86ba91a215eeda2c
sunjianbu <sunjianbu@163.com>
2018-04-12 10:11:03 +0800

    1

--- i9-defence-parent/i9-defence-api/src/main/resources/static/proj/monitoringChart/monitoringChart.js
+++ i9-defence-parent/i9-defence-api/src/main/resources/static/proj/monitoringChart/monitoringChart.js
@@ -158,8 +158,11 @@ var monitoringChartNgControl=monitoringChartNgModule.controller('monitoringChart
     //分页条件
     $scope.pageSize = 10;
     $scope.currentPage = 1;
-    //故障类型
-    $scope.type=1;
+    //图表显示隐藏状态
+    $scope.chartsStatus = false;
+    $scope.idNum = 0;
+
+    $scope.changeTimeStatu = 1;
     //初始化
     $scope.pageInit = function (){
     	var text = $scope.searchText;
@@ -206,11 +209,124 @@ var monitoringChartNgControl=monitoringChartNgModule.controller('monitoringChart
     		$scope.pages = data.data.data.loopPageNum;
     		$scope.currentPage = pageParam.currentPage;
 
-    		$scope.passagewayInit($scope.projects[0].id);
+    		if($scope.projects.length>0){
+				$scope.idNum = $scope.projects[0].id;
+				$scope.passagewayInit();
+			}
     	})
     };
     $scope.pageInit();
+    $scope.passagewayInit = function (){
+    	var text = $scope.searchText;
+    	var pageParam = {
+    			equipmentId:$scope.idNum,
+    			startDateString:$scope.startTime,
+    			endDateString:$scope.endTime,
+    			/*projectName : text,
+    			projectAddress : text,*/
+    		};
+    	
+    	httpService.post({url:'./equipment/selectEquipInfoAndData',data:pageParam,showSuccessMsg:false}).then(function(data) {  
+    		$scope.equipmentInfo = data.data.data;
+    		$scope.projectInfo = data.data;
+    		$scope.equipmentCheckArr = [];
+    		$scope.equipmentItemArr = [];
 
+    		
+    		if($scope.equipmentInfo!= null){
+    			$scope.chartsStatus = true;
+    			for(i in $scope.equipmentInfo.channelData){
+    				$scope.equipmentItemObj = {
+    		            type:'line',
+    		            stack:'10',
+    		            symbol: 'emptyCircle',
+    		            symbolSize: 10,
+    		            itemStyle:{
+    		                normal:{
+    		                    color:'#ab56dc',
+    		                }
+    		            },
+    		            lineStyle:{
+    		                normal:{
+    		                    color:'#ab56dc',
+    		                }
+    		            },
+    		        };
+    				$scope.equipmentCheckArr.push('通道'+$scope.equipmentInfo.channelData[i].channelNumber);
+    				$scope.equipmentItemObj.name='通道'+$scope.equipmentInfo.channelData[i].channelNumber;
+    				$scope.equipmentItemObj.data=$scope.equipmentInfo.channelData[i].value;
+    				$scope.equipmentItemArr.push($scope.equipmentItemObj);
+    			}
+    			$scope.option={
+    			    title:{
+    			        show:false,
+    			    },
+    			    toolbox:{
+    			        show:false,
+    			    },
+    			    grid:{
+    			        top:10,
+    			        left:60,
+    			        right:120,
+    			        bottom:30,
+    			        borderColor:'#566c93',
+    			    },
+    			    tooltip:{
+    			        trigger:'axis'
+    			    },
+    			    dataZoom:{
+    		            type: 'inside',
+    		            realtime: true,
+    		            start: 90,
+    		            end: 100,
+    		            // xAxisIndex: [0, 1]
+    			    },
+    			    legend:{
+    			    	type:'scroll',
+    			        right:0,
+    			        top:0,
+    			        orient:'vertical',
+    			        inactiveColor:'#666',
+    			        selectedMode:'single',
+    			        textStyle:{
+    			            color:'#fff',
+    			        },
+    			        data:$scope.equipmentCheckArr,
+    			        // data:['通道0','通道1','通道2','通道3','通道4','通道5','通道6','通道7'],
+    			    },
+    			    xAxis:{
+    			    	// type:'time',
+    			        axisLabel: {        
+    			            show: true,
+    			            textStyle: {
+    			                color: '#fff',
+    			            }
+    			        },
+    			        data:$scope.equipmentInfo.date,
+    			    },
+    			    yAxis:{
+    			        axisLabel: {        
+    			            show: true,
+    			            textStyle: {
+    			                color: '#fff',
+    			            }
+    			        },
+    			        splitLine:{
+    			            show:true,
+    			            lineStyle:{
+    			                color:'#4960bf',
+    			                type:'dashed'
+    			            }
+    			        },
+    			    },
+    			    series:$scope.equipmentItemArr,
+    			}
+    		}else{
+    			$scope.chartsStatus = false;
+    		}
+    		
+    	})
+    };
     //日期
     $scope.error = {};
     $scope.division = division;
@@ -229,8 +345,44 @@ var monitoringChartNgControl=monitoringChartNgModule.controller('monitoringChart
     $scope.c3 = function () {
        $scope.error.area = false;
     };
-
-    $scope.changeType = function (num) {
-    	$scope.type = num;
+    //修改分页大小
+    $scope.changePageSize = function(){
+    	$scope.currentPage = 1;
+    	$scope.pageInit();
+    }
+    //上一页
+    $scope.lastPage = function(){
+    	if ($scope.hasPrevious){
+    		$scope.currentPage -=1;
+    		$scope.pageInit();
+    	}
+    }
+    //下一页
+    $scope.nextPage = function (){
+    	if ($scope.hasNext){
+    		$scope.currentPage +=1;
+    		$scope.pageInit();
+    	}
+    }
+    //跳转
+    $scope.pageTo = function(page){
+    	$scope.currentPage = page;
+    	$scope.pageInit();
+    }
+    //条件搜索
+    $scope.subSearch = function () {
+    	$scope.pageInit();
+    }
+    //选择设备
+    $scope.checkItem = function (idNum) {
+    	$scope.idNum = idNum;
+    	$scope.passagewayInit();
+    }
+    //时间切换
+    $scope.changeTime = function () {
+    	$scope.changeTimeStatu = $scope.changeTimeStatu+1;
+    	if($scope.changeTimeStatu>3){
+    		$scope.passagewayInit();
+    	}
     }
 })
