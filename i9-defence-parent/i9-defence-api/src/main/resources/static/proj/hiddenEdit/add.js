0cdb4449310eacd6f8d2347df589c1ce9f9977f7
sunjianbu <sunjianbu@163.com>
2018-04-13 11:07:50 +0800

    1

--- i9-defence-parent/i9-defence-api/src/main/resources/static/proj/hiddenEdit/add.js
+++ i9-defence-parent/i9-defence-api/src/main/resources/static/proj/hiddenEdit/add.js
@@ -1,16 +1,33 @@
 var hiddenEditNgModule = angular.module('hiddenEditNgModule', [ 'ngResource',
-		'ngCookies', 'ui.bootstrap', 'toaster', 'app' ]);
+        'ngCookies', 'ui.bootstrap', 'toaster', 'app' ]);
 
 var hiddenEditNgCtrl = hiddenEditNgModule.controller('hiddenEditNgCtrl', function($scope,
-		$rootScope, $modalInstance, $cookieStore, $http, $window, toaster,
-		hiddenEdit,httpService) {
+        $rootScope,$modal, $modalInstance, $cookieStore, $http, $window, toaster, httpService,hiddenEdit) {
 	
 	$scope.hiddenEdit = hiddenEdit;
-	console.log($scope.hiddenEdit);
+    //页面初始化
+    /*$scope.pageInit = function (data) {
+        console.log(JSON.stringify(data));
+        var oHtml = "";
+        for(i in data){
+            oHtml += "<tr><td>"+data[i].name+"</td></tr>"
+            for(j in data[i].data){
+                oHtml += "<tr><td>"+data[i].data[j].value+"</td><td >"+data[i].data[j].dateTimeStr+"</td><td ><select class='form-control'><option value='0'>未处理</option><option value='1'>标记已处理</option><option value='2'>标记误报</option><option value='3'>标记故障</option></select></td><td ><input type='text' class='input-sm form-control' placeholder='处理详情'></td></tr>"
+            }
+        }
+        $('#tableBody').html(oHtml);
+        console.log(angular.element('#tableBody'));
+    }
+    $scope.pageInit($scope.hiddenEdit);*/
+
+
+
 	$scope.closeBtn = function() {
 		$modalInstance.dismiss('cancel');
 	}
-	$scope.check = function(statu){
+    
+   
+	/*$scope.check = function(statu){
 //		var text = $scope.editText;
     	$scope.delArray = [];
     	angular.forEach(angular.element.find(".o-checks1"), function(dom){
@@ -43,6 +60,6 @@ var hiddenEditNgCtrl = hiddenEditNgModule.controller('hiddenEditNgCtrl', functio
             }
         }, {confirmButtonText: '确定', cancelButtonText: '取消', width: 400});
     
-    }
+    }*/
 	
 });
