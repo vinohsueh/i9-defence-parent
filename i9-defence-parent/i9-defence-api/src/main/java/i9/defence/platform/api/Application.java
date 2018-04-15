252e13833a6f89fe04db3f0352e8890c955852d3
姜涛 <jiangtao19890902@sina.com>
2018-04-15 22:24:38 +0800

    spring config server

--- i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/Application.java
+++ i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/Application.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @ServletComponentScan // 扫描使用注解方式的servlet 
 public class Application extends SpringBootServletInitializer{
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
