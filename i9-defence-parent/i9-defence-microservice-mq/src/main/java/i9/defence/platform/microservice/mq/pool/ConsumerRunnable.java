2cc7b03655650856c7fc3885bf78804b38474d68
姜涛\Administrator <jiangtao19890902@sina.com>
2018-04-18 09:20:04 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-microservice-mq/src/main/java/i9/defence/platform/microservice/mq/pool/ConsumerRunnable.java
+++ i9-defence-parent/i9-defence-microservice-mq/src/main/java/i9/defence/platform/microservice/mq/pool/ConsumerRunnable.java
@@ -1,5 +1,11 @@
 package i9.defence.platform.microservice.mq.pool;
 
+import i9.defence.platform.service.UpStreamDecodeService;
+
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.annotation.Resource;
 import javax.jms.TextMessage;
 
@@ -8,36 +14,44 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import i9.defence.platform.service.UpStreamDecodeService;
-
 @Component
 public class ConsumerRunnable implements Runnable {
+
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Override
     public void run() {
         while (true) {
             try {
-                TextMessage textMessage = consumerService.receive();
+                final TextMessage textMessage = consumerService.receive();
                 // 如果数据为空就延迟3秒钟
                 if (textMessage == null) {
                     Thread.sleep(3000);
                     continue;
                 }
-                this.upStreamDecodeService.saveUpStreamDecode(textMessage.getText());
-                logger.info("save up stream decode success, data : " + textMessage.getText());
-                
-            }
-            catch (Exception e) {
+                executorService.execute(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            upStreamDecodeService.saveUpStreamDecode(textMessage.getText());
+                            logger.info("save up stream decode success, data : " + textMessage.getText());
+                        } catch (Exception e) {
+                            logger.error("save up stream decode error, ex : ", e);
+                        }
+                    }
+                });
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
-    
+
     @Resource
     private ConsumerService consumerService;
 
     @Autowired
-    private UpStreamDecodeService upStreamDecodeService;
+    private final UpStreamDecodeService upStreamDecodeService = null;
 }
