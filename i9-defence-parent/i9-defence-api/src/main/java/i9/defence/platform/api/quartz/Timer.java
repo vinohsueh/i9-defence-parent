c355b7090fe2c03f0ebaed23b9abcb1987b878e6
vinohsueh <413311010@qq.com>
2018-04-20 08:41:50 +0800

    1

---
+++ i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/quartz/Timer.java
@@ -0,0 +1,22 @@
+package i9.defence.platform.api.quartz;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/** 
+* 创建时间：2018年4月19日 上午9:50:13
+* @author  lby
+* @version  
+* 
+*/
+@Configuration          //证明这个类是一个配置文件  
+@EnableScheduling       //打开quartz定时器总开关  
+public class Timer {
+	
+	@Scheduled(cron="0/1 * *  * * ? ") //1秒执行一次
+	public void stepTimer(){  
+	    System.out.println(1);
+	    
+	}  
+}
---
+++ i9-defence-parent/i9-defence-utils/src/main/java/i9/defence/platform/utils/ReadProperties.java
@@ -0,0 +1,54 @@
+package i9.defence.platform.utils;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/** 
+ * 创建时间：2017年9月22日 下午4:38:10
+ * @author  lby
+ * @version  
+ * 
+ */
+public class ReadProperties {
+    
+    /**
+     * 默认目录
+     */
+    private final static String ROOT = "";
+    
+    /**
+     * 默认文件名字
+     */
+    private final static String DEFAULT_FILE = "property.properties";
+    
+    /**
+     * 根据key获取值
+     * @param key
+     * @return
+     */
+    public static String getKey(String key){
+        
+        return getKey(ROOT + "/" + DEFAULT_FILE, key);
+    }
+    
+    
+    /**
+     * 根据文件路径和key获取值
+     * @param fileName
+     * @param key
+     * @return
+     */
+    public static String getKey(String fileName, String key){
+        String result = "";
+        PropertiesConfiguration config;
+        try {
+            config = new PropertiesConfiguration(fileName);
+            result = config.getString(key);
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+}
