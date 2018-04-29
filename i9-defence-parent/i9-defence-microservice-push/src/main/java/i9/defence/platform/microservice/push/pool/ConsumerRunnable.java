3e4fd12bef89a35b633a9cdaac0ccbffc6d643f3
姜涛 <jiangtao19890902@gmail.com>
2018-04-29 10:42:06 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-microservice-push/src/main/java/i9/defence/platform/microservice/push/pool/ConsumerRunnable.java
+++ i9-defence-parent/i9-defence-microservice-push/src/main/java/i9/defence/platform/microservice/push/pool/ConsumerRunnable.java
@@ -1,8 +1,5 @@
 package i9.defence.platform.microservice.push.pool;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
@@ -10,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import i9.defence.platform.microservice.push.repository.TestDao;
 import i9.defence.platform.microservice.push.service.ConsumerServiceRunnable;
 
 @Component
@@ -20,10 +16,6 @@ public class ConsumerRunnable implements Runnable {
     public void run() {
         while (true) {
             try {
-                List<Map<String, Object>> list = testDao.getTestList();
-                for (Map<String, Object> map : list) {
-                    System.out.println(map);
-                }
                 TextMessage textMessage = consumerService.receive();
                 // 如果数据为空就延迟3秒钟
                 if (textMessage == null) {
@@ -36,10 +28,7 @@ public class ConsumerRunnable implements Runnable {
             }
         }
     }
-    
-    @Autowired
-    private TestDao testDao;
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
     
     @Autowired
