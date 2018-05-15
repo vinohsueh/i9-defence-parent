bb2ba93e47ebae79398b03f69411a8fcd4d91fbe
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-15 13:53:40 +0800

    1

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/bootstrap/BootStrap.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/bootstrap/BootStrap.java
@@ -35,7 +35,7 @@ public class BootStrap extends HttpServlet {
         InetSocketAddress address = new InetSocketAddress("0.0.0.0", 9000);
         try {
             bootstrap.bind(address).sync();
-            logger.info("game server start, ip address : 0.0.0.0 port : 9000");
+            logger.info("netty server start, ip address : 0.0.0.0 port : 9000");
         }
         catch (InterruptedException e) {
             e.printStackTrace();
