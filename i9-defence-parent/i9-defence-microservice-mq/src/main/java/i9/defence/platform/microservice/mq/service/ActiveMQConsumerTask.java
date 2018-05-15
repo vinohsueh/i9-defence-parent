d9a26098f40b3c17c39aaeeb823c434e889eefdd
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-15 10:59:13 +0800

    1

--- i9-defence-parent/i9-defence-microservice-mq/src/main/java/i9/defence/platform/microservice/mq/service/ActiveMQConsumerTask.java
+++ i9-defence-parent/i9-defence-microservice-mq/src/main/java/i9/defence/platform/microservice/mq/service/ActiveMQConsumerTask.java
@@ -5,6 +5,11 @@ import i9.defence.platform.service.UpStreamDecodeService;
 
 import java.util.TimerTask;
 
+/**
+ * 处理消息队列消费者通用父类
+ * @author R12
+ *
+ */
 public abstract class ActiveMQConsumerTask extends TimerTask {
 
     protected UpStreamDecodeService getUpStreamDecodeService() {
