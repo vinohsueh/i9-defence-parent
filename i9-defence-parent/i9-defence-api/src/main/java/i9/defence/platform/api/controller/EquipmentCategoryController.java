dbe341832b2313ff3c54c6ee06521bf8a9a50b2d
aqian <28937@DESKTOP-4NLT654>
2018-05-21 09:47:57 +0800

    123

--- i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/controller/EquipmentCategoryController.java
+++ i9-defence-parent/i9-defence-api/src/main/java/i9/defence/platform/api/controller/EquipmentCategoryController.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import i9.defence.platform.dao.vo.EqCategorySearchDto;
 import i9.defence.platform.model.EquipmentCategory;
+import i9.defence.platform.model.EquipmentSystemtype;
+import i9.defence.platform.model.Project;
+import i9.defence.platform.service.EqSystemCategoryService;
 import i9.defence.platform.service.EquipmentCategoryService;
+import i9.defence.platform.service.ProjectService;
 import i9.defence.platform.utils.PageBounds;
 
 /**
@@ -26,6 +30,10 @@ import i9.defence.platform.utils.PageBounds;
 public class EquipmentCategoryController {
     @Autowired
     private EquipmentCategoryService eqCategoryService;
+	@Autowired
+	private ProjectService projectService;
+	@Autowired
+	private EqSystemCategoryService eqSystemCategoryService;
     
     /**
      * 分页查询项目分类列表
@@ -118,4 +126,21 @@ public class EquipmentCategoryController {
         eqCategoryService.deleteEqCategory(Arrays.asList(ids));
         return result;
     }
+    
+	/**
+	 * @Title findEquipmentSystemCategory
+	 * @return
+	 */
+	@RequestMapping("/findEquipmentSystemCategory")
+	public HashMap<String, Object> findEquipmentSystemCategory() {
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		EquipmentCategory equipmentCategory = new EquipmentCategory();
+		List<EquipmentCategory> eqCategory = eqCategoryService.serchEqCategory(equipmentCategory);
+		List<EquipmentSystemtype> eqSystemCategory=eqSystemCategoryService.findEquipmentSystemCategory();
+		List<Project> project = projectService.findAllProject();
+		result.put("eqCategory", eqCategory);
+		result.put("eqSystemCategory", eqSystemCategory);
+		result.put("projects", project);
+		return result;
+	}
 }
