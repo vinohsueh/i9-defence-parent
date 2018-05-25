33db294fa6d4f4a1101750977a4c3bb6020c2ec9
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-25 17:32:55 +0800

    1

--- i9-defence-parent/i9-defence-mq-libraries/src/main/java/i9/defence/platform/mq/libraries/destination/ActiveMQQueueEnum.java
+++ i9-defence-parent/i9-defence-mq-libraries/src/main/java/i9/defence/platform/mq/libraries/destination/ActiveMQQueueEnum.java
@@ -2,5 +2,5 @@ package i9.defence.platform.mq.libraries.destination;
 
 public enum ActiveMQQueueEnum {
 
-    I9_BUSINESS, I9_OBSERVER, I9_PUSH, I9_CONNECT, I9_DISCONNECT;
+    I9_BUSINESS, I9_OBSERVER, I9_PUSH, I9_CONNECT, I9_DISCONNECT, I9_DEVICE_STATE;
 }
