da2a40bd23a226d6cee5af4b2377942174f6545f
vinohsueh <vinohsueh@10.10.10.105>
2018-05-27 09:12:34 +0800

    1

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/HeartbeatService.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/HeartbeatService.java
@@ -1,7 +1,12 @@
 package i9.defence.platform.socket.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONObject;
+
+import i9.defence.platform.mq.libraries.destination.ActiveMQQueueEnum;
+import i9.defence.platform.mq.libraries.producer.ActiveMQProducerService;
 import i9.defence.platform.netty.libraries.req.HeartbeatReqMessage;
 import i9.defence.platform.socket.context.ChannelPacker;
 import i9.defence.platform.socket.netty.Message;
@@ -17,8 +22,20 @@ public class HeartbeatService implements ICoreService {
         channelPacker.systemId = heartbeatReqMessage.systemId;
         channelPacker.deviceAddress = heartbeatReqMessage.deviceAddress;
         channelPacker.loop = heartbeatReqMessage.loop;
+        if (channelPacker.isLogin.get() == false) {
+        	JSONObject jsonObject = new JSONObject();
+            jsonObject.put("channelId", channelPacker.getChannelId());
+            jsonObject.put("systemId", channelPacker.systemId);
+            jsonObject.put("loop", channelPacker.loop);
+            jsonObject.put("deviceAddress", channelPacker.deviceAddress);
+            jsonObject.put("status", 1);
+            activeMQProducerService.sendMessage(ActiveMQQueueEnum.I9_DEVICE_STATE, jsonObject.toJSONString());
+        }
         channelPacker.isLogin.set(true);
     }
+    
+    @Autowired
+    private ActiveMQProducerService activeMQProducerService;
     
 //    private static final Logger logger = LoggerFactory.getLogger(HeartbeatService.class);
 }
