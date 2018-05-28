563f95c64542c69988053fa11d07968cb365fd69
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-28 17:34:39 +0800

    test

---
+++ i9-defence-parent/testers/i9-defence-activemq-test/src/main/java/i9/defence/platform/activemq/ActiveMQMultipleQueueNameBase.java
@@ -0,0 +1,48 @@
+package i9.defence.platform.microservice.mq;
+
+import i9.defence.platform.mq.libraries.consumer.ActiveMQConsumerService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = SpringConfiguration.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ActiveMQMultipleQueueNameBase extends AbstractJUnit4SpringContextTests {
+    
+    @Autowired
+    private ActiveMQConsumerService activeMQConsumerService;
+
+    @Test
+    public void main() throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    ActiveMQQueue destination = new ActiveMQQueue("i9.test.01");
+                    System.out.println("111111111111111111111");
+                    activeMQConsumerService.receive(destination);
+                }
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    ActiveMQQueue destination = new ActiveMQQueue("i9.test.02");
+                    System.out.println("111111111111111111111");
+                    activeMQConsumerService.receive(destination);
+                }
+            }
+        });
+        System.in.read();
+    }
+}
