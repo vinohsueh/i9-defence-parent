778e9976af0f04b268cf3c7c8e2c3922a7543f92
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-28 17:51:20 +0800

    1

--- i9-defence-parent/testers/i9-defence-activemq-test/src/main/java/i9/defence/platform/activemq/ActiveMQMultipleQueueNameTest.java
+++ i9-defence-parent/testers/i9-defence-activemq-test/src/main/java/i9/defence/platform/activemq/ActiveMQMultipleQueueNameTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class ActiveMQMultipleQueueNameBase {
+public class ActiveMQMultipleQueueNameTest {
     
     @Autowired
     private ActiveMQConsumerService activeMQConsumerService;
