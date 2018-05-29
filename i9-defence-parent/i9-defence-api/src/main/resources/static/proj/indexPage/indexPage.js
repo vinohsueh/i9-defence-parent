941aea5ac684d540d598a7a3eb62c33417d559d1
sunjianbu <sunjianbu@163.com>
2018-05-29 16:00:42 +0800

    1

--- i9-defence-parent/i9-defence-api/src/main/resources/static/proj/indexPage/indexPage.js
+++ i9-defence-parent/i9-defence-api/src/main/resources/static/proj/indexPage/indexPage.js
@@ -83,6 +83,14 @@ var indexPageNgControl=indexPageNgModule.controller('indexPageNgControl',functio
 		};
 		httpService.post({url:'./project/selectProject',data:pageParam,showSuccessMsg:false}).then(function(data) { 
 			$scope.projectList = data.data.data;
+			//音频播放判断
+			var myAudio = document.getElementById('myAudio');
+			if(data.data.warningIds.length>0){
+				myAudio.play();
+			}else{
+				myAudio.pause();
+			}
+
 			var markItem = {};
 			
 			for(i in $scope.projectList){
