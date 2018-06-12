083b9c6f8b2956502399750b870600043f916c8a
姜涛\Administrator <jiangtao19890902@sina.com>
2018-06-12 16:02:34 +0800

    1

--- i9-defence-parent/i9-defence-netty-libraries/src/main/java/i9/defence/platform/netty/libraries/RespMessageBuilder.java
+++ i9-defence-parent/i9-defence-netty-libraries/src/main/java/i9/defence/platform/netty/libraries/RespMessageBuilder.java
@@ -14,11 +14,12 @@ public class RespMessageBuilder {
         
         ByteBuffer byteBuffer = ByteBuffer.allocate(6 + data.length);
         byteBuffer.put((byte) 0x10);
-        byte type = 0x04;
         if (messageEncodeConvert.getType() == 0x00 || messageEncodeConvert.getType() == 0xff) {
-        	type = messageEncodeConvert.getType();
+            byteBuffer.put(messageEncodeConvert.getType());
         }
-        byteBuffer.put(type);
+        else if (messageEncodeConvert.getType() == 0x03) {
+            byteBuffer.put((byte) 0x04);
+        }
         byteBuffer.putInt(index);
 //        byteBuffer.put((byte) data.length);
         byteBuffer.put(data);
