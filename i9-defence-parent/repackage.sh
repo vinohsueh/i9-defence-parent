ceb08ad513b40e9d7a5f4ae4e020ccf323bce0cc
姜涛 <jiangtao19890902@gmail.com>
2018-07-29 22:02:45 +0800

    repackage.sh

--- i9-defence-parent/repackage.sh
+++ i9-defence-parent/repackage.sh
@@ -11,10 +11,12 @@ rm -rf /tmp/ROOT.war
 rm -rf /tmp/i9-defence-microservice-mq.war
 rm -rf /tmp/i9-defence-microservice-observer.war
 rm -rf /tmp/i9-defence-microservice-push.war
+rm -rf /tmp/i9-defence-microservice-sms.war
 rm -rf /tmp/i9-defence-socket.war
 
 cp -R $M_PATH/i9-defence-microservice-config/target/ROOT.war /tmp/ROOT.war
 cp -R $M_PATH/i9-defence-microservice-mq/target/i9-defence-microservice-mq.war /tmp/i9-defence-microservice-mq.war
 cp -R $M_PATH/i9-defence-microservice-observer/target/i9-defence-microservice-observer.war /tmp/i9-defence-microservice-observer.war
 cp -R $M_PATH/i9-defence-microservice-push/target/i9-defence-microservice-push.war /tmp/i9-defence-microservice-push.war
+cp -R $M_PATH/i9-defence-microservice-sms/target/i9-defence-microservice-sms.war /tmp/i9-defence-microservice-sms.war
 cp -R $M_PATH/i9-defence-socket/target/i9-defence-socket.war /tmp/i9-defence-socket.war
