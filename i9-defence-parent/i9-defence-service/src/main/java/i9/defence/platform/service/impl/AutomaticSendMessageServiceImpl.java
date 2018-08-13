1f3608ce8604075ff4f97eb66b63efd3570d9377
xuepuhao <877783692@qq.com>
2018-08-13 19:18:01 +0800

    111

--- i9-defence-parent/i9-defence-service/src/main/java/i9/defence/platform/service/impl/AutomaticSendMessageServiceImpl.java
+++ i9-defence-parent/i9-defence-service/src/main/java/i9/defence/platform/service/impl/AutomaticSendMessageServiceImpl.java
@@ -1,7 +1,14 @@
 package i9.defence.platform.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import i9.defence.platform.model.Equipment;
 import i9.defence.platform.model.Project;
@@ -35,22 +42,27 @@ public class AutomaticSendMessageServiceImpl implements AutomaticSendMessageServ
         Project project = projectService.getProjectById(equipment.getProjectId());
         //2.1判断发送状态 0：不发送 1：发送
 		if(1==project.getSendStatus()) {
-			StringBuffer clientNamesBuffer = new StringBuffer("[");
-	        StringBuffer clientPhonesBuffer = new StringBuffer("[\"");
-	        StringBuffer clientSignNamesBuffer = new StringBuffer("[\"");
+			JSONArray clientNamesjsonArray = new JSONArray();
+			JSONObject clientPhonesjsonObject = new JSONObject();
+			JSONObject clientSignNamesjsonObject = new JSONObject();
+			List<String> PhonsList = new ArrayList<String>();
+			List<String> SignNameList = new ArrayList<String>();
 	        String[] recipients = project.getRecipients().split(",");
 	        String[] recipientphones = project.getRecipientphones().split(",");
 	        for(int i=0;i<recipients.length;i++) {
-	        	if(i==recipients.length-1) {
-	        		clientNamesBuffer.append("{\"name\":\"").append(recipients[i]).append("\","+"\"project\":\""+project.getProjectName()+"\","+"\"postion\":\""+equipment.getEquipmentRemarks()+"\","+"\"equipmentType\":\""+equipment.getEquipmentCategory().getEqCategoryName()+"\"").append("}]");
-	        		clientPhonesBuffer.append(recipientphones[i]).append("\"]");
-	        		clientSignNamesBuffer.append("合极电气").append("\"]");
-	        	}else {
-	        		clientNamesBuffer.append("{\"name\":\"").append(recipients[i]).append("\","+"\"project\":\""+project.getProjectName()+"\","+"\"postion\":\""+equipment.getEquipmentRemarks()+"\","+"\"equipmentType\":\""+equipment.getEquipmentCategory().getEqCategoryName()+"\"},");
-	        		clientPhonesBuffer.append(recipientphones[i]).append("\",\"");
-	        		clientSignNamesBuffer.append("合极电气").append("\",\"");
-	        	}
+	        	JSONObject clientNamesJson = new JSONObject();
+	        	clientNamesJson.put("name", recipients[i]);
+	        	clientNamesJson.put("project", project.getProjectName());
+	        	clientNamesJson.put("postion", equipment.getEquipmentRemarks());
+	        	clientNamesJson.put("equipmentType",equipment.getEquipmentCategory().getEqCategoryName());
+	        	clientNamesjsonArray.add(clientNamesJson);
+	        	PhonsList.add(recipientphones[i]); 
+	        	SignNameList.add("合极电气"); 
 			}
+	        clientPhonesjsonObject.put("phones", PhonsList);
+	        clientSignNamesjsonObject.put("signNames", SignNameList);
+	        Map<String,Object> phonesMap=(Map<String, Object>)clientPhonesjsonObject.parse(clientPhonesjsonObject.toJSONString());
+	        Map<String,Object> signNamesMap=(Map<String, Object>)clientSignNamesjsonObject.parse(clientSignNamesjsonObject.toJSONString());
 			//2.2获取设备发送类型(0:报警，1:离线，2：隐患)
 			String[] splitType = project.getSendType().split(",");
 			//2.3将String数组转换成int数组
@@ -62,14 +74,14 @@ public class AutomaticSendMessageServiceImpl implements AutomaticSendMessageServ
 			for(int h=0;h<arr.length;h++) {
 				//2.5若SendType=0并且alertStatus=1则发送报警短信
 				if(0==arr[h] && 1 == equipmentType) {
-					AliyunUtil.sendInfo(AliyunSMSEnum.WANING, clientPhonesBuffer.toString(), clientNamesBuffer.toString(), clientSignNamesBuffer.toString());
+					AliyunUtil.sendInfo(AliyunSMSEnum.WANING,phonesMap.get("phones").toString(), clientNamesjsonArray.toJSONString(),signNamesMap.get("signNames").toString() );
 				//2.6若SendType=2并且alertStatus=2则发送隐患短信
 				}else if(2==arr[h] && 2 == equipmentType) {
-					AliyunUtil.sendInfo(AliyunSMSEnum.HIDDENDANGER, clientPhonesBuffer.toString(), clientNamesBuffer.toString(), clientSignNamesBuffer.toString());
+					AliyunUtil.sendInfo(AliyunSMSEnum.HIDDENDANGER,phonesMap.get("phones").toString(), clientNamesjsonArray.toJSONString(),signNamesMap.get("signNames").toString() );
 				//2.7若SendType=0并且status=1则发送离线短信
 				}
 				if(1==arr[h] && 0 == equipmentType) {
-					AliyunUtil.sendInfo(AliyunSMSEnum.OUTOFLINE, clientPhonesBuffer.toString(), clientNamesBuffer.toString(), clientSignNamesBuffer.toString());
+					AliyunUtil.sendInfo(AliyunSMSEnum.OUTOFLINE,phonesMap.get("phones").toString(), clientNamesjsonArray.toJSONString(),signNamesMap.get("signNames").toString() );
 				}
 				
 			}
