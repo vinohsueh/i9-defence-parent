4ea4ae0f8ad98bcbd5f3e6e5382cff6f52782239
xuepuhao <877783692@qq.com>
2018-09-20 16:20:44 +0800

    update

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/bootstrap/BootStrap.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/bootstrap/BootStrap.java
@@ -1,7 +1,14 @@
 package i9.defence.platform.socket.bootstrap;
 
+import java.net.InetSocketAddress;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import i9.defence.platform.socket.netty.SocketServerInitializer;
-import i9.defence.platform.socket.util.HTTPUtil;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
@@ -9,15 +16,6 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("serial")
 public class BootStrap extends HttpServlet {
@@ -48,18 +46,18 @@ public class BootStrap extends HttpServlet {
 
     public void batchSetDeviceStatusToOffline() {
         // TODO 在这里增加访问URL
-        String requestUrl = "http://103.248.102.21:8080/equipment/updateAllEquipmentStatus";
-        boolean can = false;
-        do {
-            try {
-                HashMap<String, Object> result = HTTPUtil.sendPost(requestUrl, new HashMap<String, String>());
-                Integer code = (Integer) result.get("code");
-                if (code != null && code == 0) {
-                    can = true;
-                }
-            } catch (Exception e) {
-                can = false;
-            }
-        } while (!can);
+//        String requestUrl = "http://103.248.102.21:8080/equipment/updateAllEquipmentStatus";
+//        boolean can = false;
+//        do {
+//            try {
+//                HashMap<String, Object> result = HTTPUtil.sendPost(", new HashMap<String, String>());
+//                Integer code = (Integer) result.get("code");
+//                if (code != null && code == 0) {
+//                    can = true;
+//                }
+//            } catch (Exception e) {
+//                can = false;
+//            }
+//        } while (!can);
     }
 }
