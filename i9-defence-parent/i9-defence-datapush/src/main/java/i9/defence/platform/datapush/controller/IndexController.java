9bb8ae73df0ccc719516601634dffe2ac41a7160
姜涛 <jiangtao19890902@sina.com>
2018-10-12 10:47:45 +0800

    ftl

---
+++ i9-defence-parent/i9-defence-datapush/src/main/java/i9/defence/platform/datapush/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.shyroke.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    @RequestMapping(value = "/login")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        
+        List<String> userList=new ArrayList<String>();
+        userList.add("admin");
+        userList.add("user1");
+        userList.add("user2");
+        
+        modelAndView.addObject("userList", userList);
+        return modelAndView;
+    }
+    
+}
