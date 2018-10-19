229a9d6932bd715e949c3e62156ce9b1055f5fdc
root <root@localhost>
2018-10-19 21:25:50 +0800

    1

--- i9-defence-parent/i9-defence-datapush/src/main/java/i9/defence/platform/datapush/ServerRun.java
+++ i9-defence-parent/i9-defence-datapush/src/main/java/i9/defence/platform/datapush/ServerRun.java
@@ -1,9 +1,14 @@
 package i9.defence.platform.datapush;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 服务启动类
@@ -13,8 +18,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableTransactionManagement
 @EnableScheduling
+@Configuration
 @SpringBootApplication
 public class ServerRun {
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServerRun.class, args);
--- i9-defence-parent/i9-defence-datapush/src/test/java/i9/defence/platform/datapush/controller/DeviceControllerTest.java
+++ i9-defence-parent/i9-defence-datapush/src/test/java/i9/defence/platform/datapush/controller/DeviceControllerTest.java
@@ -1,10 +1,27 @@
 package i9.defence.platform.datapush.controller;
 
+import i9.defence.platform.datapush.utils.HttpResult;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration
 public class DeviceControllerTest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void test() {
+        HttpResult<?> httpResult = this.restTemplate.getForObject(
+                "http://localhost:9999/baseAPI/deleteDevice.sapi", HttpResult.class);
+        System.out.println(httpResult.getMessage());
+    }
 }
