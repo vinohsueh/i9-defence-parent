d5fe0e5f91113a3b3d77a96de0210f57cbe0232a
jiangtao <jiangtao19890902@sina.com>
2018-10-19 21:07:53 +0800

    注释

--- i9-defence-parent/i9-defence-datapush/src/main/java/i9/defence/platform/datapush/controller/DeviceController.java
+++ i9-defence-parent/i9-defence-datapush/src/main/java/i9/defence/platform/datapush/controller/DeviceController.java
@@ -5,13 +5,13 @@ import i9.defence.platform.datapush.service.DeviceService;
 import i9.defence.platform.datapush.utils.HttpResponseUtil;
 import i9.defence.platform.datapush.utils.HttpResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/baseAPI/")
 public class DeviceController {
 
---
+++ i9-defence-parent/i9-defence-datapush/src/test/java/i9/defence/platform/datapush/controller/DeviceControllerTest.java
@@ -0,0 +1,10 @@
+package i9.defence.platform.datapush.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DeviceControllerTest {
+}
