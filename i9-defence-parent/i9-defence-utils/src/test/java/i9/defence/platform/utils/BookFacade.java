3301be98f42be1a7e70eba6aa6553268bc451166
姜涛 <jiangtao19890902@sina.com>
2018-11-02 14:00:21 +0800

    测试类

---
+++ i9-defence-parent/i9-defence-utils/src/test/java/i9/defence/platform/utils/BookFacade.java
@@ -0,0 +1,10 @@
+package i9.defence.platform.utils;
+
+import java.util.UUID;
+
+public class BookFacade {
+
+    public void print() {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
