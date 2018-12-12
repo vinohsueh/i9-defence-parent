3b6c610dfae471433ecb1ca396fe5488080b7aac
姜涛 <jiangtao19890902@sina.com>
2018-12-12 16:09:46 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/UpStreamService.java
+++ i9-defence-parent/i9-defence-socket/src/main/java/i9/defence/platform/socket/service/impl/UpStreamService.java
@@ -1,12 +1,5 @@
 package i9.defence.platform.socket.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.alibaba.fastjson.JSONObject;
-
 import i9.defence.platform.mq.libraries.destination.ActiveMQQueueEnum;
 import i9.defence.platform.mq.libraries.producer.ActiveMQProducerService;
 import i9.defence.platform.netty.libraries.DataParseUtil;
@@ -20,6 +13,15 @@ import i9.defence.platform.socket.exception.BusinessException;
 import i9.defence.platform.socket.netty.Message;
 import i9.defence.platform.socket.service.ICoreService;
 import i9.defence.platform.socket.util.ChannelConnectedService;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Service
 public class UpStreamService implements ICoreService {
@@ -56,9 +58,9 @@ public class UpStreamService implements ICoreService {
         reqMessage.showInfo();
         for (DataMessage dataMessage : reqMessage.dataList) {
             dataMessage.showInfo();
-            logger.info(
-                    "解析数据包体, 数据类型 : " + dataMessage.type + ", hex : " + EncryptUtils.bytesToHexString(dataMessage.data)
-                            + ", 值 : " + DataParseUtil.parseDataValue(dataMessage.type, dataMessage.data));
+            logger.info("解析数据包体, 数据类型 : " + dataMessage.type + ", hex : "
+                    + EncryptUtils.bytesToHexString(dataMessage.data) + ", 值 : "
+                    + DataParseUtil.parseDataValue(dataMessage.type, dataMessage.data));
         }
         // 清理上行数据
         JSONObject jsonObject = reqMessage.toJSONObject();
@@ -90,14 +92,32 @@ public class UpStreamService implements ICoreService {
             byte channel = item.getByte("channel");
             if (channel == 19 || channel == 20 || channel == 21 || channel == 22) {
                 Short value = item.getShort("value");
-                item.put("value", (value * 0.1f - 50));
+                item.put("value", this.setScaleFormat(value * 0.1f - 50));
             }
             if (channel == 23 || channel == 24 || channel == 25 || channel == 26 || channel == 27 || channel == 28) {
                 Short value = item.getShort("value");
-                item.put("value", (value * 0.1f));
+                item.put("value", this.setScaleFormat(value * 0.1f));
+            }
+            if (channel == 29 || channel == 30 || channel == 31) {
+                Integer value = item.getInteger("value");
+                item.put("value", this.setScaleFormat(value * 0.001f));
+            }
+            if (channel == 35 || channel == 36 || channel == 37) {
+                Short value = item.getShort("value");
+                item.put("value", this.setScaleFormat(value * 0.1f));
+            }
+            if (channel == 38 || channel == 39 || channel == 40) {
+                Integer value = item.getInteger("value");
+                item.put("value", this.setScaleFormat(value * 0.001f));
             }
         }
         logger.info("power, new : " + jsonObject.toJSONString());
+    }
+
+    private double setScaleFormat(double f) {
+        BigDecimal bigDecimal = new BigDecimal(f);
+        double r = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        return r;
     }
 
     private final static Logger logger = LoggerFactory.getLogger(UpStreamService.class);
