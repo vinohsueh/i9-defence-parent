8b4b1ec2d10f8bab6a7194ee76280c85ce8f7b36
姜涛 <jiangtao19890902@gmail.com>
2018-12-20 22:18:16 +0800

    1

--- i9-defence-parent/i9-defence-aliyun-docker/src/main/docker/package.sh
+++ i9-defence-parent/i9-defence-aliyun-docker/src/main/docker/package.sh
@@ -2,6 +2,6 @@ mvn package docker:build
 
 docker images
 
-docker run -p 8080:8080 -t springboot/spring-boot-docker
+docker run -p 8080:8080 -t -i -d springboot/i9-defence-aliyun-docker
 
 http://127.0.0.1:8080/
