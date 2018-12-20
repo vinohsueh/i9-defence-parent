c984cf1498b9c2b55555d0d76ab708d94c4a41ba
姜涛 <jiangtao19890902@gmail.com>
2018-12-20 21:11:51 +0800

    Change-Id: I0000000000000000000000000000000000000000

---
+++ i9-defence-parent/i9-defence-aliyun-docker/src/main/docker/package.sh
@@ -0,0 +1,7 @@
+mvn package docker:build
+
+docker images
+
+docker run -p 8080:8080 -t springboot/spring-boot-docker
+
+http://127.0.0.1:8080/
---
+++ i9-defence-parent/i9-defence-aliyun-docker/src/main/java/i9/defence/platform/aliyun/docker/controller/IndexController.java
@@ -0,0 +1,13 @@
+package i9.defence.platform.aliyun.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker!";
+    }
+}
