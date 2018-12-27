87d5c688860d2a2ad145d484320ded93ca91156a
姜涛 <jiangtao19890902@gmail.com>
2018-12-27 20:17:18 +0800

    1

--- i9-defence-parent/i9-defence-aliyun-docker/src/main/java/i9/defence/platform/aliyun/docker/message/AliyunMessageCallback.java
+++ i9-defence-parent/i9-defence-aliyun-docker/src/main/java/i9/defence/platform/aliyun/docker/message/AliyunMessageCallback.java
@@ -1,5 +1,7 @@
 package i9.defence.platform.aliyun.docker.message;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ import i9.defence.platform.aliyun.docker.service.UpStreamService;
 @Component
 public class AliyunMessageCallback implements MessageCallback {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AliyunMessageCallback.class);
+
     @Override
     public Action consume(MessageToken messageToken) {
         Message m = messageToken.getMessage();
-        System.out.println("receive message from " + m);
+        LOGGER.info("接收阿里云数据 : " + m);
         this.upStreamService.saveUpStream(m.getMessageId(), m.getTopic(), new String(m.getPayload()));
         return MessageCallback.Action.CommitSuccess;
     }
--- i9-defence-parent/i9-defence-aliyun-docker/src/main/java/i9/defence/platform/aliyun/docker/message/AliyunMessageClient.java
+++ i9-defence-parent/i9-defence-aliyun-docker/src/main/java/i9/defence/platform/aliyun/docker/message/AliyunMessageClient.java
@@ -2,6 +2,8 @@ package i9.defence.platform.aliyun.docker.message;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,8 @@ import com.aliyun.openservices.iot.api.message.api.MessageClient;
 
 @Component
 public class AliyunMessageClient {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(AliyunMessageClient.class);
 
     @Value("${aliyun.appKey}")
     private String appKey;
@@ -19,7 +23,7 @@ public class AliyunMessageClient {
     @Value("${aliyun.appSecret}")
     private String appSecret;
 
-    @Value("${aliyun.productKey}")
+    @Value("${aliyun.appSecret}")
     private String productKey;
 
     @Autowired
@@ -28,6 +32,10 @@ public class AliyunMessageClient {
     @PostConstruct
     public void init() {
         String endPoint = "https://" + productKey + ".iot-as-http2.cn-shanghai.aliyuncs.com";
+        LOGGER.info("加载阿里云配置文件, appKey : " + this.appKey);
+        LOGGER.info("加载阿里云配置文件, appSecret : " + this.appSecret);
+        LOGGER.info("加载阿里云配置文件, productKey : " + this.productKey);
+        LOGGER.info("加载阿里云配置文件, endPoint : " + endPoint);
         // 连接配置
         Profile profile = Profile.getAppKeyProfile(endPoint, appKey, appSecret);
         // 构造客户端
